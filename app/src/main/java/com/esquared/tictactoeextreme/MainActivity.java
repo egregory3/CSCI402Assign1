a50207ffccc0c4827115460d18501f75a533c1e2
Eric Gregory <egregory3@my.apsu.edu>
2020-02-02 21:18:55 -0600

    Very basic landing screen created: All text plain white, cursive font style.  Game button text is font size 28sp.

--- app/src/main/java/com/esquared/tictactoeextreme/MainActivity.java
+++ app/src/main/java/com/esquared/tictactoeextreme/MainActivity.java
@@ -2,7 +2,10 @@ package com.esquared.tictactoeextreme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((TextView)findViewById(R.id.creditsLabel)).setText(Html.fromHtml("Brought to you by e<sup><font size=14px>2</font></sup>"));
     }
 }
