c8fe14ff524e96dc35d95b159c81983977f96021
Eric Gregory <egregory3@my.apsu.edu>
2020-02-09 17:14:39 -0600

    Working version of Devils Tic Tac toe and main menu link to devils

--- app/src/main/java/com/esquared/tictactoeextreme/MainActivity.java
+++ app/src/main/java/com/esquared/tictactoeextreme/MainActivity.java
@@ -3,8 +3,10 @@ package com.esquared.tictactoeextreme;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ((TextView)findViewById(R.id.creditsLabel)).setText(Html.fromHtml("Brought to you by e<sup><font size=14px>2</font></sup>"));
+        findViewById(R.id.Game1Btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlayGame();
+            }
+        });
     }
+    //Open the PlayGame activity
+    private void PlayGame() {
+        Intent intent = new Intent(getApplicationContext(),
+                play_devils.class);
+        startActivity(intent);
+    }
+
 }
